bbf99146cb97b00dff7e7744e2445c1975db611f
Lincoln Hoey Moore <johlinco@gmail.com>
2021-07-04 05:38:03 -0400

    first commit

---
+++ public/script.js
@@ -0,0 +1,62 @@
+//DOM ELEMENTS
+var button = document.getElementById('btn');
+var plant_name_input = document.getElementById('plant_name_input')
+var harvest_time_input= document.getElementById('harvest_time_input')
+var thinning_time_input = document.getElementById('thinning_time_)input')
+var thinning_spacing_input = document.getElementById('thinning_spacing_input')
+var water_frequency_input = document.getElementById('water_frequency_input')
+var planted_date_input = document.getElementById('planted_date_input')
+
+
+//VARIABLES
+
+const api = 'http://localhost:3000/item';
+
+
+
+//FUNCTIONS
+
+
+function postRequest(data) {
+	return new Promise((resolve, reject) => {
+		let request = new XMLHttpRequest();
+		request.open('POST', api);
+		request.onreadystatechange = () => {
+			if (request.readystate === 4) {
+				if (request.status === 201) {
+					resolve(JSON.parse(request.reponse));
+				} else {
+					reject(JSON.parse(request.response));
+				}
+			}
+		};
+		request.setRequestHeader('Content-Type', 'application/json');
+		request.send(JSON.stringify(data));
+	});
+}
+
+
+function addDays(date, days) {
+	var result = new Date(date);
+	result.setDate(result.getDate() + days);
+	return result;
+}
+
+//EVENT LISTENERS
+btn.addEventListener('click', ($event) => {
+	
+
+	const post = {
+		plant_name: plant_name_input.value,
+		harvest_time: harvest_time_input.value,
+		thinning_time: thinning_spacing_input.value,
+		thinning_spacing: thinning_spacing_input.value,
+		water_frequency: water_frequency_input.value,
+		planted_date: planted_date_input.value
+	};
+
+	postRequest(post);
+
+	console.log(post);
+
+});
